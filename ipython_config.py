51ad0e5229e3112119602dd5b95cab3187eedabe
richard <richardwangwang@gmail.com>
2016-05-26 12:35:38 +0800

    Update ipython config.

--- ipython_config.py
+++ ipython_config.py
@@ -1,8 +1,17 @@
-try:
-    from manage import *
-    initialize_settings()
-except:
-    pass
+from ufs_tools.libtool import include_all
+
+#     logging.basicConfig(level=logging.DEBUG)
+include_all(__file__, "server_base_packages")
+from djangoautoconf import DjangoAutoConf
+
+DjangoAutoConf.set_settings_env()
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+
+from django.conf import settings
+
 # Password to use for web authentication
 # c = get_config()
 # c.NotebookApp.password = 'sha1:96b6954d678f:630c0a102b0b7e498dfed0b972f1b5ec19719c02'
