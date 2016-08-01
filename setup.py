873f22439c78164f4bf465dd27546a2aef8e89ee
richard <richardwangwang@gmail.com>
2016-08-01 13:16:43 +0800

    Refine setup.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # from setuptools import find_packages
+import json
 import os
 import pprint
 import sys
@@ -6,6 +7,9 @@ import django
 import pkg_resources
 import pytz
 import zope.interface
+import _tkinter
+from os.path import dirname
+from cx_Freeze import setup
 
 from ufs_tools import get_folder
 from ufs_tools.basic_lib_tool import include
@@ -14,144 +18,151 @@ include_all(__file__, "server_base_packages")
 
 
 from djangoautoconf.auto_conf_utils import get_module_path
-
-import _tkinter
-from os.path import dirname
-from cx_Freeze import setup
-
-include_all(__file__, "server_base_packages")
-
 from djangoautoconf.setting_utils.app_folders import AppFolderUtil
-
-
-try:
-    os.remove("local/total_settings.py")
-except:
-    pass
-
-from django_build.app_freeze_config import gen_executable_list, get_pytz_files, \
-    get_iconizer_resources, create_executable_from_app_name
-
-
-root_folder = get_folder(__file__)
-u = AppFolderUtil(root_folder)
-for folder in u.enum_app_folders():
-    include(folder)
-
+from django_build.app_freeze_config import gen_executable_list, get_iconizer_resources, create_executable_from_app_name
 from django_build.django_setup import DjangoCxFreezeBuildSpecGenerator
-
-# Create data.db for SQLITE so build process can run with SQLITE
-os.environ["POSTGRESQL_ROOT"] = ""
-
 from djangoautoconf import DjangoAutoConf
 
-DjangoAutoConf.set_settings_env()
 
-from django.conf import settings
-
-
-django.setup()
+total_setting_file_path = "local/total_settings.py"
 
 
-###########################
-# Add python module that is not automatically included in the build below. Such as Django app
-# file with special names other than: models, urls, admin, views etc.
-###########################
-includes = [
-    # os.environ["DJANGO_SETTINGS_MODULE"], #rootapp
-    "PyQt4.QtCore",
-    # "pkg_resources",  # Used by pytz to load time zone info in zoneinfo folder
-    "yaml",
-    # For Cherrypy
-    # "django.contrib.messages",
-    "email",
-    "email.message",
-    "cherrypy",
-    "iconizer",
-    # "zope",
-    # "zope.interface",
-    "django.core.management",
-    "django.core.management.commands.syncdb",
-]
-
-app_list = [
-    # 'starter',
-    'manage',
-    'cherrypy_server',
-]
-
-include_files = []
-
-include_files.extend(get_iconizer_resources())
-
-include_files.extend([
-    ("local", "local"),
-    (get_module_path(pytz), "pytz"),
-    (get_module_path(zope.interface), "zope/interface"),
-    (get_module_path(pkg_resources), "pkg_resources"),
-    ("server_base_packages/distutils", "distutils"),
-    # ("server_base_packages/pkg_resources", "pkg_resources"),
-    # #("libs/allauth/fixtures/initial_data.json", "initial_data.json"),
-    # ("libs/zlib1.dll", "libs/zlib1.dll"),
-    # ("libs/regex2.dll", "libs/regex2.dll"),
-    # ("libs/magic1.dll", "libs/magic1.dll"),
-    # ("tornado_app.bat", "tornado.bat"),
-    # ("activate_app.bat", "activate.bat"),
-])
-
-excludefiles = []
-zip_includes = []  # get_pytz_files()
-build_exe_dir = "../build/%s" % os.path.basename(os.path.dirname(__file__))
-
-build_exe_params = {
-    "includes": includes,
-    'include_files': include_files,
-    "bin_excludes": excludefiles,
-    "build_exe": build_exe_dir,
-    "zip_includes": zip_includes,
-    # https://bitbucket.org/anthony_tuininga/cx_freeze/issues/127/collectionssys-error
-    'excludes': ['collections.abc',
-                 'collections.sys',
-                 'cStringIO.errno',
-                 'cStringIO.sys',
-                 'distutils.archive_util',
-                 ]
-    # "packages": find_packages(),
-    # "create_shared_zip": False,
-}
+def remove_if_exists(file_path):
+    try:
+        os.remove(file_path)
+    except:
+        pass
 
 
-# os.system(os.path.join(root_folder, "scripts/syncdb.bat"))
-os.system(sys.executable + " ./manage.py migrate")
-os.system(os.path.join(root_folder, "scripts/collectstatic.bat"))
-os.system(os.path.join(root_folder, "scripts/collectcmd.bat"))
-os.system(sys.executable + " ./manage.py dump_settings")
+def main():
+    remove_if_exists(total_setting_file_path)
 
-# Need to remove port_v3 for QT for cx_freeze when packaging PyQt
+    root_folder = get_folder(__file__)
+    u = AppFolderUtil(root_folder)
+    for folder in u.enum_app_folders():
+        include(folder)
+
+    # Create data.db for SQLITE so build process can run with SQLITE
+    os.environ["POSTGRESQL_ROOT"] = ""
+
+    DjangoAutoConf.set_settings_env()
+
+    from django.conf import settings
+
+    django.setup()
+
+    ###########################
+    # Add python module that is not automatically included in the build below. Such as Django app
+    # file with special names other than: models, urls, admin, views etc.
+    ###########################
+    includes = [
+        "PyQt4.QtCore",
+        "yaml",
+        # For Cherrypy
+        # "django.contrib.messages",
+        "email",
+        "email.message",
+        "cherrypy",
+        "iconizer",
+        "django.core.management",
+        "django.core.management.commands.syncdb",
+    ]
+
+    app_list = [
+        # 'starter',
+        'manage',
+        'cherrypy_server',
+    ]
+
+    additional_config_json_file_path = "local/additional_exe_config.json"
+    os.path.exists(additional_config_json_file_path)
+    additional_exe_config_file = open(additional_config_json_file_path)
+    additional_config = json.load(additional_exe_config_file)
+
+    app_list.extend(additional_config["additional_apps"])
+
+    include_files = []
+
+    include_files.extend(get_iconizer_resources())
+
+    include_files.extend([
+        ("local", "local"),
+        # Required for pytz, otherwise, although build will be done, there will be timezone not found error in runtime
+        # Used by pytz to load time zone info in zoneinfo folder
+        (get_module_path(pytz), "pytz"),
+        (get_module_path(zope.interface), "zope/interface"),
+        (get_module_path(pkg_resources), "pkg_resources"),
+        ("server_base_packages/distutils", "distutils"),
+        # ("server_base_packages/pkg_resources", "pkg_resources"),
+        # #("libs/allauth/fixtures/initial_data.json", "initial_data.json"),
+        # ("libs/zlib1.dll", "libs/zlib1.dll"),
+        # ("libs/regex2.dll", "libs/regex2.dll"),
+        # ("libs/magic1.dll", "libs/magic1.dll"),
+        # ("tornado_app.bat", "tornado.bat"),
+        # ("activate_app.bat", "activate.bat"),
+    ])
+
+    build_exe_dir = "../build/%s" % os.path.basename(os.path.dirname(__file__))
+
+    build_exe_params = {
+        "includes": includes,
+        'include_files': include_files,
+        "bin_excludes": [],
+        "build_exe": build_exe_dir,
+        "zip_includes": [],
+        # https://bitbucket.org/anthony_tuininga/cx_freeze/issues/127/collectionssys-error
+        'excludes': ['collections.abc',
+                     'collections.sys',
+                     'cStringIO.errno',
+                     'cStringIO.sys',
+                     'distutils.archive_util',
+                     ]
+        # "packages": find_packages(),
+        # "create_shared_zip": False,
+    }
+
+    # os.system(os.path.join(root_folder, "scripts/syncdb.bat"))
+    os.system(sys.executable + " ./manage.py migrate")
+    os.system(os.path.join(root_folder, "scripts/collectstatic.bat"))
+    os.system(os.path.join(root_folder, "scripts/collectcmd.bat"))
+    os.system(sys.executable + " ./manage.py dump_settings")
+
+    # Need to remove port_v3 for QT for cx_freeze when packaging PyQt
+    set_env_var_for_tkinter_in_exe()
+
+    DjangoCxFreezeBuildSpecGenerator().gen_spec(settings, build_exe_params)
+
+    final_script_list = gen_executable_list(app_list)
+    final_script_list.append(create_executable_from_app_name("starter"))
+    # final_script_list.append(create_executable_from_app_name("starter", base="Win32GUI"))
+    pprint.pprint(build_exe_params)
+    setup(
+        version="0.1",  # This is required or build process will have exception.
+        description="application starter",
+        options={"build_exe": build_exe_params},
+        executables=final_script_list,
+        # include_package_data=True,
+        # package_data = {'':['*.*', 'templates/*.*']},
+        # packages = find_packages(),
+    )
+
+    remove_if_exists(total_setting_file_path)
 
 
-# Workarround for cx_freeze packaging cherrypy
+def set_env_var_for_tkinter_in_exe():
+    """
+    For fixing error of the following:
+    _tkinter.TclError: Can't find a usable init.tcl in the following directories:
+    c:/python27/lib/tcl8.5 D:/work/venv/django18/lib/tcl8.5 D:/work/venv/lib/tcl8.5 D:/work/venv/django18/library D:/work/venv/library D:/work/venv/tcl8.5.15/library D:/work/tcl8.5.15/library
+    This probably means that Tcl wasn't installed properly.
+    :return: None
+    """
+    python_dir = dirname(dirname(_tkinter.__file__))
+    tcl_lib_path_name = "tcl/tcl" + _tkinter.TCL_VERSION
+    tk_lib_path_name = "tcl/tk" + _tkinter.TCL_VERSION
+    os.environ["TCL_LIBRARY"] = os.path.join(python_dir, tcl_lib_path_name)
+    os.environ["TK_LIBRARY"] = os.path.join(python_dir, tk_lib_path_name)
 
 
-python_dir = dirname(dirname(_tkinter.__file__))
-tcl_lib_path_name = "tcl/tcl" + _tkinter.TCL_VERSION
-tk_lib_path_name = "tcl/tk" + _tkinter.TCL_VERSION
-os.environ["TCL_LIBRARY"] = os.path.join(python_dir, tcl_lib_path_name)
-os.environ["TK_LIBRARY"] = os.path.join(python_dir, tk_lib_path_name)
-
-
-DjangoCxFreezeBuildSpecGenerator().gen_spec(settings, build_exe_params)
-
-final_script_list = gen_executable_list(app_list)
-final_script_list.append(create_executable_from_app_name("starter"))
-# final_script_list.append(create_executable_from_app_name("starter", base="Win32GUI"))
-pprint.pprint(build_exe_params)
-setup(
-    version="0.1",  # This is required or build process will have exception.
-    description="application starter",
-    options={"build_exe": build_exe_params},
-    executables=final_script_list,
-    # include_package_data=True,
-    # package_data = {'':['*.*', 'templates/*.*']},
-    # packages = find_packages(),
-)
+if __name__ == "__main__":
+    main()
