cbff962a05adc79e902a9c45713e2ae52d96206b
Richard <richardwangwang@gmail.com>
2016-08-06 16:28:49 +0800

    Added freeze support.

--- manage.py
+++ manage.py
@@ -1,15 +1,17 @@
 import logging
 import sys
+import os
 
+from multiprocessing import freeze_support
 from ufs_tools.libtool import include_all
 from ufs_tools.python_app_utils.base import AppBase
-
+os.environ["POSTGRESQL_ROOT"] = "others/pgsql"
 
 if __name__ == "__main__":
     # logging.basicConfig(level=logging.DEBUG)
     AppBase().add_default_module_path()
     from djangoautoconf import DjangoAutoConf
     DjangoAutoConf.set_settings_env()
-
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
+    # The following is required for freeze app for django
+    freeze_support()
+    DjangoAutoConf.exe()
