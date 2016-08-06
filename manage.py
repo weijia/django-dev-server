3af8c9fb6efac1177811fbdd1abbe62700ae32ea
Richard <richardwangwang@gmail.com>
2016-08-06 16:41:29 +0800

    Remove default flag for postgresql.

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
 import logging
 import sys
-import os
 
 from multiprocessing import freeze_support
-from ufs_tools.libtool import include_all
 from ufs_tools.python_app_utils.base import AppBase
-os.environ["POSTGRESQL_ROOT"] = "others/pgsql"
+
 
 if __name__ == "__main__":
     # logging.basicConfig(level=logging.DEBUG)
